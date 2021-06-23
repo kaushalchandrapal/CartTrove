9d4ce3a2c5449ec532f60fa86d8b5dac26ad7952
Kaushal Chandrapal <kaushalchandrapal@Kaushals-MacBook-Pro.local>
2021-06-23 23:37:45 +0530

    Mongo Setup finished

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import colors from 'colors';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        });
+
+        console.log(`MongoDb Connected: ${conn.connection.host}`.bgCyan.white.bold);
+    } catch (error) {
+        console.error(`Error: ${error.message}`.bgMagenta.white.bold);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen';
+// import CartScreen from './Screens/CartScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to CartTrove</h1>
-    </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
+          {/* <Route path='/cart/:id?' component={CartScreen} /> */}
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
