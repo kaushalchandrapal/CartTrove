302b259d99bf253ffc276cfcc8a793f1b792b841
Kaushal Chandrapal <kaushalchandrapal@Kaushals-MacBook-Pro.local>
2021-07-13 22:28:25 +0530

    Before order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
   return (
@@ -16,12 +19,15 @@ const App = () => {
       <Header/>
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile/' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import { userDetailsReducer,
     userRegisterReducer, 
     userUpdateProfileReducer
 } from './reducers/userReducers';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -17,23 +18,29 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
     ? JSON.parse(localStorage.getItem('cartItems'))
-    : []
+    : [];
 
 const userInfoFromStorage = localStorage.getItem('userInfo') 
     ? JSON.parse(localStorage.getItem('userInfo')) 
-    : null
+    : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    : {};
 
 const initialState = {
     cart: {
         cartItems: cartItemsFromStorage,
-        userLogin: userInfoFromStorage,
-    }
-}
+        shippingAddress: shippingAddressFromStorage,
+    },
+    userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
